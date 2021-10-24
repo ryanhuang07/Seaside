09afe29c4952c59a4a5dddd14422b6e5870bb917
rh6501 <rh6507@gmail.com>
2021-10-24 16:30:37 -0400

    Second Activity and some other bullshit 2

--- app/src/main/java/com/example/seaside/AddEvent.java
+++ app/src/main/java/com/example/seaside/AddEvent.java
@@ -1,16 +1,44 @@
 package com.example.seaside;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddEvent extends AppCompatActivity {
+
+    EditText title, description, location, time, date, volunteers;
+    Button submit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_event);
 
 
+        submit = (Button) findViewById(R.id.eventSubmit);
+        title = (EditText) findViewById(R.id.eventTitle);
+        description = (EditText) findViewById(R.id.eventDescription);
+        location = (EditText) findViewById(R.id.eventLocation);
+        time = (EditText) findViewById(R.id.eventTime);
+        date = (EditText) findViewById(R.id.eventDate);
+        volunteers = (EditText) findViewById(R.id.eventVolunteers);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseAccess controller = new DatabaseAccess(AddEvent.this);
+
+                boolean success = controller.addEvent(title.getText().toString(), description.getText().toString(), location.getText().toString(), time.getText().toString() + "&" + date.getText().toString(), Integer.parseInt(volunteers.getText().toString()));
+                String message = success == true ? "Success" : "Failure adding to the database";
+
+                Toast.makeText(AddEvent.this, String.valueOf(message), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
+
